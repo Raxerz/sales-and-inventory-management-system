af9bc06b9cf322079eda8b5677b3006399cf4496
Rajath <rajathkrishna.jagana@dish.com>
2020-06-13 14:31:05 +0530

    Modified db to remote

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","root","inventory");
+$con = mysqli_connect("sql12.freemysqlhosting.net:3306","sql12348063","3GH2PY8nNC","sql12348063");
 
 // Check connection
 if (mysqli_connect_errno())
