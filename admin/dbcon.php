f87359e4bbaee66894f7d88c4d21a7713a4b156a
Rajath <rajathkrishna.jagana@dish.com>
2020-06-13 15:20:55 +0530

    Removed database connection details

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("sql12.freemysqlhosting.net:3306","sql12348063","3GH2PY8nNC","sql12348063");
+$con = mysqli_connect("<database-host-path:<port>","<username>","<password>","<database-name>");
 
 // Check connection
 if (mysqli_connect_errno())
